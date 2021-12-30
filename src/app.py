4a5bcba7cd77a34717488632146365cc0b331a29
Topourtis Dimitrios <dtopourtis@gmail.com>
2021-12-30 09:08:05 +0200

    second commit

--- src/app.py
+++ src/app.py
@@ -6,19 +6,10 @@ app=Flask(__name__)
 
 @app.route("/index")
 @app.route("/")
-@app.route("/<name>")
-def root(name=None):
-    return render_template("index.html",myname=name)
+def root():
+    return render_template("index.html")
 
-@app.route("/books/")
-def books():
 
-    with open("mybooks.json") as json_file:
-        jdict=json.load(json_file)
-        
-
-    return render_template("books.html",jdict=jdict)
- 
 
 if __name__=="__main__":
     app.run(debug=True)
