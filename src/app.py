87b357a42ef403021f74914f3bc359a3b8508636
Topourtis Dimitrios <dtopourtis@gmail.com>
2021-12-30 17:11:15 +0200

    created neserarly templates

--- src/app.py
+++ src/app.py
@@ -1,13 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask.json import htmlsafe_dump
 import json
 
 app=Flask(__name__)
 
-@app.route("/index")
+@app.route("/index/")
 @app.route("/")
 def root():
     return render_template("index.html")
+
+@app.route("/signup/")
+def signup():
+    return render_template("signup.html")
+
+@app.route("/login/")
+def login():
+    return render_template("login.html")
+
+@app.route("/logout/")
+def logout():
+    return redirect(url_for("root"))
+
+@app.route("/new_article/")
+def new_article():
+    return render_template("new_article.html")
 
 
 
